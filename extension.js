63e42b0123c5be0c5d9e5da83189238e05659d2f
Vergeture <Subract@outlook.fr>
2015-04-28 18:25:59 +0200

    Fork for Ethernel

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 (function () {
     //Link location of your fork so you don't have to modify so many things.
-    var fork = "Yemasthui";
+    var fork = "Vergeture";
 		
     //Define our function responsible for extending the bot.
     function extend() {
@@ -35,14 +35,14 @@
          */
 
         bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'topic',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat("/me http://minilien.fr/a0pr8p");
                 }
             }
         };
